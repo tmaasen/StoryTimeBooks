1c78af6645a662c3d62ae6e328ad5dee892a071c
Tanner-Maasen <tmaasen@gmail.com>
2020-02-25 02:41:18 -0600

    login and signup pages

---
+++ DATABASE-PROJECT/Project/vue-ui/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import store from './store/store'
+
+import '@/assets/style.css'
+//import './registerServiceWorker'
+
+import GoogleAuth from '@/config/google_oAuth.js'
+const gauthOption = {
+  clientId: '707231563844-e5cpkqrlt62gncmj6b84of5sml9lp8g9.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GoogleAuth, gauthOption)
+Vue.config.productionTip = false
+
+new Vue({
+   router,
+   store,
+  render: h => h(App)
+}).$mount('#app')
