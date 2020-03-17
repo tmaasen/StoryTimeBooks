3e17705ee942e3e0f3138c0fa20b3c33da7773b2
haseokwoo <seokwooha@student.cofo.edu>
2020-03-17 03:11:32 -0500

    footer without vue

---
+++ DATABASE-PROJECT/StoryTimeBooks/database/migrations/2020_03_16_184909_sogu.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Sogu extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //We use this up() function to create tables and define columns.
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //ex)if this happens, then drop the table.
+    }
+}
