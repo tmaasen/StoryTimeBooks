aa54abbb9f8fdea61e71809fb2c89128580a8ccf
Tanner-Maasen <tmaasen@gmail.com>
2020-04-28 17:15:42 -0500

    restructured project / optimized

--- Mini-Project/database-master/SalesmanReference/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module salesmanreference {
-	requires java.persistence;
-	requires java.instrument;
-	requires java.sql;
-	requires servlet.api;
-
-	opens model;
-}
--- Mini-Project/database-master/interestingsearch.sql
+++
@@ -1,9 +0,0 @@
-with EmployeesWhoReportToManager6 as
-(
-	select *
-    from employee
-    where ManagerID=6
-    
-)
-select *
-from EmployeesWhoReportToManager6
