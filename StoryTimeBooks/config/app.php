415d696de72023664157387ad02a8f55af20f4fd
Tanner-Maasen <tmaasen@gmail.com>
2020-05-30 09:40:55 -0500

    updated all packages

--- StoryTimeBooks/config/app.php
+++ StoryTimeBooks/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://ec2-54-234-59-6.compute-1.amazonaws.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
